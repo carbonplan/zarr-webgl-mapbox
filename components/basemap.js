1f1d895d6195e11d41f3efb071955e92cb14e772
freeman-lab <the.freeman.lab@gmail.com>
2021-08-14 11:12:34 -0700

    add colormap

--- components/basemap.js
+++ components/basemap.js
@@ -25,7 +25,7 @@ const Basemap = () => {
         },
       })
     }
-  }, [map])
+  }, [])
 
   useEffect(() => {
     if (map.getLayer('land')) {
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,60 @@
 import { useState } from 'react'
 import { Box } from 'theme-ui'
-import { Slider, Dimmer, Toggle } from '@carbonplan/components'
+import { Slider, Dimmer, Toggle, Meta } from '@carbonplan/components'
 import { Canvas } from '../lib'
 import Layers from '../components/layers'
 import style from '../components/style'
 
 const Index = () => {
   const [display, setDisplay] = useState(true)
-  const [brightness, setBrightness] = useState(1)
+  const [opacity, setOpacity] = useState(1)
+  const [clim, setClim] = useState([-20, 30])
 
   return (
-    <Box sx={{ position: 'absolute', top: 0, bottom: 0, width: '100%' }}>
-      <Canvas style={style} zoom={0} center={[0, 0]} debug={true}>
-        <Layers display={display} brightness={brightness} />
-      </Canvas>
-      <Toggle
-        sx={{ position: 'absolute', top: 20, right: 20 }}
-        value={display}
-        onClick={() => setDisplay((prev) => !prev)}
-      />
-      <Dimmer sx={{ position: 'absolute', bottom: 20, right: 20 }} />
-      <Slider
-        min={0}
-        max={1}
-        step={0.01}
-        sx={{ width: '200px', position: 'absolute', top: 20, left: 20 }}
-        value={brightness}
-        onChange={(e) => setBrightness(parseFloat(e.target.value))}
-      />
-    </Box>
+    <>
+      <Meta />
+      <Box sx={{ position: 'absolute', top: 0, bottom: 0, width: '100%' }}>
+        <Canvas style={style} zoom={2} center={[0, 0]} debug={false}>
+          <Layers display={display} opacity={opacity} clim={clim} />
+        </Canvas>
+        <Toggle
+          sx={{ position: 'absolute', top: 20, right: 20 }}
+          value={display}
+          onClick={() => setDisplay((prev) => !prev)}
+        />
+        <Slider
+          min={0}
+          max={1}
+          step={0.01}
+          sx={{ width: '200px', position: 'absolute', top: 20, left: 20 }}
+          value={opacity}
+          onChange={(e) => setOpacity(parseFloat(e.target.value))}
+        />
+        <Slider
+          min={-20}
+          max={30}
+          step={0.1}
+          sx={{ width: '200px', position: 'absolute', top: 40, left: 20 }}
+          value={clim[0]}
+          onChange={(e) =>
+            setClim((prev) => [parseFloat(e.target.value), prev[1]])
+          }
+        />
+        <Slider
+          min={-20}
+          max={30}
+          step={0.1}
+          sx={{ width: '200px', position: 'absolute', top: 60, left: 20 }}
+          value={clim[1]}
+          onChange={(e) =>
+            setClim((prev) => [prev[0], parseFloat(e.target.value)])
+          }
+        />
+        <Dimmer
+          sx={{ position: 'absolute', right: [13], bottom: [17, 17, 15, 15] }}
+        />
+      </Box>
+    </>
   )
 }
 
